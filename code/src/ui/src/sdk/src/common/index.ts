a701466842a836d80a9788a43d0aa4a0cbaaa755
Bryce Curtis <curtis.bryce@gmail.com>
2023-03-23 19:48:10 -0400

    Remove sdk directory

--- code/src/ui/src/sdk/src/common/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./node";
-export * from "./props";
-export * from "./shade";
--- code/src/ui/src/sdk/src/organisms/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./organism";
-export * from "./organisms";
-export * from "./dataTables";
-export * from "./hero";
-export * from "./primaryNav";
-export * from "./secondaryNav";
-export * from "./footerAndCopyright";
