ea0ce1d89c68f54aaacbc2348a7a2ffb94e44e68
Evan Kountouris <evangk6@gmail.com>
2023-03-24 16:30:50 -0400

    Complete Lists Components

---
+++ code/src/ui/src/pages/components/ListComponent.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Avatar, Checkbox, FormControlLabel, Grid } from '@mui/material';
+
+import ErrorIcon from '@mui/icons-material/Error';
+import { Link } from 'react-router-dom';
+
+
+interface Props {
+    type: number
+    title: string
+    isStyle2?: boolean;
+    hasAvatar?: boolean
+    hasImg?: boolean
+    hasIcon?: boolean
+    isLarge?: boolean
+    hasCheckbox?: boolean
+    isClickable?: boolean
+}
+
+export const ListComponent: React.FC<Props> = ({ type, title, isStyle2, hasAvatar,
+        hasImg, hasIcon, isLarge, hasCheckbox, isClickable }) => {
+
+    const body = <div className="body2">
+        Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum.
+    </div>
+
+    const listTitle = <>
+        {!(type === 2) || <div className="overline">OVERLINE</div> }
+        {isStyle2 || <div className="body2">List Title</div>}
+        {!isStyle2 || <div className="subtitle1">List Title</div>}
+        {!(type === 3) || body }
+    </>
+
+    const listDecor = <>
+        {!hasAvatar || <Avatar/>}
+        {!hasImg || isLarge
+        || <img alt='list-style-1' src="/image-1.jpeg" style={{objectFit: "cover",width: "60px", height: "60px"}}/>}
+        {!hasImg || !isLarge
+        || <img alt='list-style-1' src="/image-1.jpeg" style={{objectFit: "cover",width: "100px", height: "60px"}}/>}
+        {!hasIcon  || isLarge 
+        || <ErrorIcon color='error'/>}
+        {!hasIcon  || !isLarge 
+        || <ErrorIcon color='error' fontSize='large'/>}
+    </>
+
+
+    
+  
+
+    return (
+        <div className="sample">
+            <div className="subtitle1">{title}</div>
+            <div className="list">
+                {isClickable || listDecor}
+                {!isClickable || <Link to={''} style={{ color: "inherit", textDecoration: 'none' }}>{listDecor}</Link>}
+                <div className="list-body">
+                    <Grid container spacing={6}>
+                        <Grid item xs={6}>
+                            {isClickable || listTitle}
+                            {!isClickable || <Link to={''} style={{ color: "inherit", textDecoration: 'none' }}>{listTitle}</Link>}
+                        </Grid>
+                        {!hasCheckbox
+                        || <Grid item xs={4}><FormControlLabel control={<Checkbox defaultChecked />} label="One" /></Grid>}
+                    </Grid>
+                </div>
+            </div>
+        </div>
+    )
+}
--- code/src/ui/src/pages/components/ListsDoubleComponent.tsx
+++ code/src/ui/src/pages/components/ListsDoubleComponent.tsx
@@ -1,19 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HeadingSection } from '../content/HeadingSection';
 import { ExampleSection } from '../content/ExampleSection';
-import { Avatar } from '@mui/material';
+import { FormControlLabel, Radio, RadioGroup } from '@mui/material';
+import { SettingsSection } from '../content/SettingsSection';
+
+import { ListComponent } from './ListComponent';
 
 
 interface Props {
 }
 
 export const ListsDoubleComponent: React.FC<Props> = ({ }) => {
+
+
+    const [isClickable, setIsClickable] = useState(false)
+    async function handleChange(event: any): Promise<void> {
+        const value = event.target.value;
+        if (value === "clickable") {
+            setIsClickable(true)
+            return
+        }
+        setIsClickable(false)
+    }
+
     return (
         <div className="content">
             <HeadingSection title='Desktop' heading='Lists - Double Line'></HeadingSection>
+            <SettingsSection>
+                <RadioGroup 
+                    aria-labelledby="isClickable-button-group-label"
+                    name="isClickable-buttons-group"
+                    defaultValue="non-clickable"
+                    onChange={handleChange}
+                >
+                    <FormControlLabel value="non-clickable" control={<Radio />} label="Non-Clickable"/>
+                    <FormControlLabel value="clickable" control={<Radio />} label="Clickable List"/>
+                </RadioGroup>
+            </SettingsSection>
             <ExampleSection>
-                
+                <section>
+                    <div className="row">
+                        <div className="col-6">
+                            <h6>Double Line Lists without Checkbox</h6>
+                            <ListComponent isClickable={isClickable} type={2} title={'Single Line - Style 1'}></ListComponent>
+                            <ListComponent isClickable={isClickable} type={2} title={'List - Style 2'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasAvatar type={2} title={'Style 1 - with Avatar'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasAvatar type={2} title={'Style 2 - with Avatar'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasImg type={2} title={'Style 1 - with Square Image'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasImg type={2} title={'Style 2 - with Square Image'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasImg isLarge type={2} title={'Style 1 - with Wide Image'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasImg isLarge type={2} title={'Style 2 - with Wide Image'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasIcon type={2} title={'Style 1 - with Icon'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasIcon type={2} title={'Style 2 - with Icon'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasIcon isLarge type={2} title={'Style 1 - with Large Icon'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasIcon isLarge type={2} title={'Style 2 - with Large Icon'} isStyle2></ListComponent>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div className="row">
+                        <div className="col-6">
+                            <h6>Double Line Lists with Checkbox</h6>
+                            <ListComponent isClickable={isClickable} hasCheckbox type={2} title={'Single Line - Style 1'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox type={2} title={'List - Style 2'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasAvatar type={2} title={'Style 1 - with Avatar'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasAvatar type={2} title={'Style 2 - with Avatar'} isStyle2></ListComponent>                            <ListComponent hasCheckbox hasImg type={2} title={'Style 1 - with Square Image'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasImg type={2} title={'Style 2 - with Square Image'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasImg isLarge type={2} title={'Style 1 - with Wide Image'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasImg isLarge type={2} title={'Style 2 - with Wide Image'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasIcon type={2} title={'Style 1 - with Icon'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasIcon type={2} title={'Style 2 - with Icon'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasIcon isLarge type={2} title={'Style 1 - with Large Icon'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasIcon isLarge type={2} title={'Style 2 - with Large Icon'} isStyle2></ListComponent>
+                        </div>
+                    </div>
+                </section>
             </ExampleSection>
+
         </div>
     )
 }
