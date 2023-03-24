72b2f9d6795db808cc952a3a9261e13c46982632
Evan Kountouris <evangk6@gmail.com>
2023-03-24 16:30:50 -0400

    Complete Lists Components

--- code/src/ui/src/pages/components/ListsTripleComponent.tsx
+++ code/src/ui/src/pages/components/ListsTripleComponent.tsx
@@ -1,19 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HeadingSection } from '../content/HeadingSection';
 import { ExampleSection } from '../content/ExampleSection';
-import { Avatar } from '@mui/material';
+import { FormControlLabel, Radio, RadioGroup } from '@mui/material';
+import { SettingsSection } from '../content/SettingsSection';
+
+import { ListComponent } from './ListComponent';
 
 
 interface Props {
 }
 
 export const ListsTripleComponent: React.FC<Props> = ({ }) => {
+
+
+    const [isClickable, setIsClickable] = useState(false)
+    async function handleChange(event: any): Promise<void> {
+        const value = event.target.value;
+        if (value === "clickable") {
+            setIsClickable(true)
+            return
+        }
+        setIsClickable(false)
+    }
+
     return (
         <div className="content">
             <HeadingSection title='Desktop' heading='Lists - Triple Line'></HeadingSection>
+            <SettingsSection>
+                <RadioGroup 
+                    aria-labelledby="isClickable-button-group-label"
+                    name="isClickable-buttons-group"
+                    defaultValue="non-clickable"
+                    onChange={handleChange}
+                >
+                    <FormControlLabel value="non-clickable" control={<Radio />} label="Non-Clickable"/>
+                    <FormControlLabel value="clickable" control={<Radio />} label="Clickable List"/>
+                </RadioGroup>
+            </SettingsSection>
             <ExampleSection>
-                
+                <section>
+                    <div className="row">
+                        <div className="col-6">
+                            <h6>Triple Line Lists without Checkbox</h6>
+                            <ListComponent isClickable={isClickable} type={3} title={'Triple Line - Style 1'}></ListComponent>
+                            <ListComponent isClickable={isClickable} type={3} title={'List - Style 2'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasAvatar type={3} title={'Style 1 - with Avatar'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasAvatar type={3} title={'Style 2 - with Avatar'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasImg type={3} title={'Style 1 - with Square Image'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasImg type={3} title={'Style 2 - with Square Image'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasImg isLarge type={3} title={'Style 1 - with Wide Image'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasImg isLarge type={3} title={'Style 2 - with Wide Image'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasIcon type={3} title={'Style 1 - with Icon'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasIcon type={3} title={'Style 2 - with Icon'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasIcon isLarge type={3} title={'Style 1 - with Large Icon'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasIcon isLarge type={3} title={'Style 2 - with Large Icon'} isStyle2></ListComponent>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div className="row">
+                        <div className="col-6">
+                            <h6>Triple Line Lists with Checkbox</h6>
+                            <ListComponent isClickable={isClickable} hasCheckbox type={3} title={'Single Line - Style 1'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox type={3} title={'List - Style 2'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasAvatar type={3} title={'Style 1 - with Avatar'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasAvatar type={3} title={'Style 2 - with Avatar'} isStyle2></ListComponent>                            <ListComponent hasCheckbox hasImg type={3} title={'Style 1 - with Square Image'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasImg type={3} title={'Style 2 - with Square Image'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasImg isLarge type={3} title={'Style 1 - with Wide Image'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasImg isLarge type={3} title={'Style 2 - with Wide Image'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasIcon type={3} title={'Style 1 - with Icon'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasIcon type={3} title={'Style 2 - with Icon'} isStyle2></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasIcon isLarge type={3} title={'Style 1 - with Large Icon'}></ListComponent>
+                            <ListComponent isClickable={isClickable} hasCheckbox hasIcon isLarge type={3} title={'Style 2 - with Large Icon'} isStyle2></ListComponent>
+                        </div>
+                    </div>
+                </section>
             </ExampleSection>
+
         </div>
     )
 }
