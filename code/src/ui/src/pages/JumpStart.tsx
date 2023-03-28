ffb5bbceb5f0aa58ea34073e0df38d0423daab3f
Bryce Curtis <curtis.bryce@gmail.com>
2023-03-28 10:50:14 -0400

    Update welcome pages

--- code/src/ui/src/pages/JumpStart.tsx
+++ code/src/ui/src/pages/JumpStart.tsx
@@ -1,54 +1,30 @@
 import React from 'react';
 import SystemCard from '../components/SystemCard';
-import { Grid } from '@mui/material';
-
+import { ExampleSection } from './content/ExampleSection';
+import { HeadingSection } from './content/HeadingSection';
 
 interface Props {
     user: any;
 }
 
-const JumpStart: React.FC<Props> = ({user}) => {
-  
+const JumpStart: React.FC<Props> = ({ user }) => {
+
     return (
-            <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="overline-large">
-                        Sample and Template Design Systems
-                    </div>
-                    <h1>Jump Start</h1>
-                    <div className="subtitle1">
-                        Design Systems
-                    </div>
-                    <p>
-                        Explore our sample design systems.  You can duplicate theme
-                            to make your own or start from scratch to build your own systems.
-                    </p>
-                </div>
+        <div>
+            <HeadingSection title="Sample and Template Design System" heading="Jump Start">
+                Explore our sample design systems.  You can duplicate theme
+                to make your own or start from scratch to build your own systems.
+            </HeadingSection>
+            <div className="top40" />
+            <ExampleSection title="Sample Design Systems">
+            <div style={{ display: "flex", gap: "20px" }}>
+                <SystemCard
+                    name="Sample"
+                    title="Sample"
+                    onClickHandler={async (event, name) => { window.location.href = "/designSystem/Sample" }}
+                />
             </div>
-            <div className="row top40">
-                <div className="col-12">
-                    <h5>Sample Design Systems</h5>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-12">
-                    <div className="card-area">
-                        <Grid
-                            container
-                            spacing={3}
-                        >
-                            <Grid item xs={9} sm={7} md={5}>
-                                <SystemCard 
-                                    name="Sample"
-                                    title="Sample"
-                                    onClickHandler={async (event,name)=>{window.location.href="/designSystem/Sample"}}
-                                />
-                            </Grid>
-                        </Grid>
-                    </div>
-                </div>
-            </div>
+            </ExampleSection>
         </div>
     );
 }
