3a29bb96faa544e3aa04adfd5454d74bdf6c3d39
Bryce Curtis <curtis.bryce@gmail.com>
2023-03-30 15:21:05 -0400

    Update accordion

--- code/src/ui/src/pages/components/AccordionComponent.tsx
+++ code/src/ui/src/pages/components/AccordionComponent.tsx
@@ -1,49 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-//import './AccordionComponent.css';
+import { HeadingSection } from '../content/HeadingSection';
+import { ColorModeSelector } from '../content/ColorModeSelector';
 
 interface Props {
 }
 
 export const AccordionComponent: React.FC<Props> = () => {
+
+    const [colorMode, setColorMode] = useState<string>("primary");
+
     return (
-        <div className="content">
-            <section>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <Accordion>
-                                <AccordionSummary
-                                    expandIcon={<ExpandMoreIcon />}
-                                    aria-controls="panel1a-content"
-                                    id="panel1a-header"
-                                >
-                                    <div className="subtitle1">This is my question</div>
-                                </AccordionSummary>
-                                <AccordionDetails>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                                    </p>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                                    </p>
-                                </AccordionDetails>
-                            </Accordion>
-                        </div>
-                    </div>
-                </div>
-            </section>
+        <div>
+            <HeadingSection title="Desktop Components" heading="Accordions" />
+            <ColorModeSelector colorMode={colorMode} setColorMode={setColorMode}>
+                <Accordion style={{ margin: "40px" }}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <div className="subtitle1">This is my question</div>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                        </p>
+                    </AccordionDetails>
+                </Accordion>
+            </ColorModeSelector>
         </div>
     )
 }
