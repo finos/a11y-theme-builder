0824bfa9a3d882dc492aaa51d2035154d51f8e72
Bryce Curtis <curtis.bryce@gmail.com>
2023-04-06 15:27:33 -0400

    [TB] Update color mode selector #44

--- code/src/ui/src/pages/content/ColorModeSelector.tsx
+++ code/src/ui/src/pages/content/ColorModeSelector.tsx
@@ -13,23 +13,25 @@ export interface ColorModeSelector {
 
 export const ColorModeSelector: React.FC<ColorModeSelector> = ({ colorMode, setColorMode, children }) => {
 
-    const backgroundColor:any = {primary: "var(--primary)", black: "black", white: "white"}
-    const color:any = {primary: "var(--on-primary)", black: "white", white: "black"}
+    const backgroundColor:any = {default: "var(--background)", black: "white", white: "black"}
+    // const color:any = {default: "var(--on-primary)", black: "black", white: "white"}
 
     const style = {
         border: "1px dotted black",
-        color: color[colorMode],
+        //color: color[colorMode],
         backgroundColor: backgroundColor[colorMode],
         padding: "10px",
         marginTop: "24px",
-    }   
+    }
+
+    console.log("style=",style);
 
     return (
         <>
-        <InputLabel>Background Colors</InputLabel>
-        <Typography variant="caption">View components on different backgrounds</Typography>
+        <InputLabel>Color Variants</InputLabel>
+        <Typography variant="caption">View components as default, black or white variants</Typography>
         <RadioGroup onChange={(event) => setColorMode(event.target.value)} defaultValue={colorMode} value={colorMode}>
-            <FormControlLabel value="primary" control={<Radio size="small"/>} label="Primary Color"/>
+            <FormControlLabel value="default" control={<Radio size="small"/>} label="Default"/>
             <FormControlLabel value="black" control={<Radio size="small"/>} label="Black"/>
             <FormControlLabel value="white" control={<Radio size="small"/>} label="White"/>
         </RadioGroup>
