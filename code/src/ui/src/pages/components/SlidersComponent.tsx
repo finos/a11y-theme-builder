61471cc765f9c0d92d339e40e1e3cc84636d0deb
Evan Kountouris <evangk6@gmail.com>
2023-04-07 10:15:28 -0400

    add sliders component

---
+++ code/src/ui/src/pages/components/SlidersComponent.tsx
@@ -0,0 +1,26 @@
+﻿/*
+ * Copyright (c) 2023 Discover Financial Services
+ * Licensed under MIT License. See License.txt in the project root for license information
+ */
+import React from 'react';
+import { HeadingSection } from '../content/HeadingSection';
+import { Slider } from '@mui/material';
+import { ExampleSection } from '../content/ExampleSection';
+
+
+interface Props {
+}
+
+export const SlidersComponent: React.FC<Props> = () => {
+    return (
+        <div className="content">
+            <HeadingSection title='Desktop' heading='Switch'></HeadingSection>
+            <ExampleSection>
+                <div className="caption">Slider</div>
+                <Slider defaultValue={30} valueLabelDisplay="auto" sx={{maxWidth:400}}/>
+                <div className="caption top40">Range Slider</div>
+                <Slider defaultValue={[20,40]} valueLabelDisplay="auto" sx={{maxWidth:400}}/>
+            </ExampleSection>
+        </div>
+    )
+}
