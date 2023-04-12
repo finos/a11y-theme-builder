9555ab1bc3eaa57aac12eefac2eb8f419b962bcb
Aaron Reed <aaronreed708@gmail.com>
2023-04-12 14:40:30 -0400

    discoverfinancial/a11y-theme-builder#96: added code to use an anchor to show sample text.  Code will now pull computed styling from the anchor to get underline value

--- code/src/ui/src/components/ComputedHotlinkColorSwatch.tsx
+++ code/src/ui/src/components/ComputedHotlinkColorSwatch.tsx
@@ -17,25 +17,38 @@ interface Props {
     containerClassName?: string;
 }
 
+// This component displays a sample hotlink, a color sample, and
+//  color information in a ColorSwatch.  It pulls this information
+//  from the computed styling of the reference HTML element.  In
+//  this specific component, that is a HTMLAnchorElement that
+//  acts as the sample hotlink.
 export const ComputedHotlinkColorSwatch: React.FC<Props> = ({className, sampleText, label, style, containerClassName}) => {
 
-    const colorDisplayRef = useRef<HTMLDivElement>(null);
+    const colorLinkRef = useRef<HTMLAnchorElement>(null);
 
-    const [_backgroundColorRGB, _setBackgroundColorRGB] = useState<string>("");
+    const [_colorRGB, _setColorRGB] = useState<string>("");
     const [_opacity, _setOpacity] = useState<string>("1");
+    const [_underline, _setUnderline] = useState<boolean>(true);
 
     useEffect(() => {
         if (!className) return;
-        const displayDiv = colorDisplayRef.current;
-        if (!displayDiv) {
+        const displayAnchor = colorLinkRef.current;
+        if (!displayAnchor) {
             throw new Error("color swatch missing DOM information");
         }
-        const cssObj = window.getComputedStyle(displayDiv, null);
-        const colorRGBA = processColorIntoRGBA(cssObj.backgroundColor, cssObj.opacity);
-        _setBackgroundColorRGB(colorRGBA);
+        const cssObj = window.getComputedStyle(displayAnchor, null);
+        const colorRGBA = processColorIntoRGBA(cssObj.color, cssObj.opacity);
+        // store off the RGB color, opacity and underline information
+        //  so that they can be used to display these details
+        //  in the color swatch
+        _setColorRGB(colorRGBA);
         _setOpacity(cssObj.opacity);
+        _setUnderline(cssObj.textDecorationLine === "underline")
     }, [className])
 
+    // Pulls the RGB values from the given color, uses the given
+    //  opacity value for alpha, and then builds a string from
+    //  these values in the format of 'rgba(r,g,b,a)'
     const processColorIntoRGBA = (color: string, opacity: string) : string => {
         if (!color) return "";
         const colorObj = colord(color);
@@ -51,14 +64,18 @@ export const ComputedHotlinkColorSwatch: React.FC<Props> = ({className, sampleTe
     return (
         <div className={`color-swatch ${containerClassName} text-swatch`} style={style}>
             {label && <div className="small-semibold">{label}</div>}
-            <div ref={colorDisplayRef} className={`color-box ${className}`} />
+            <div className={`color-box ${className}`} />
             <div>
                 <div className="small-semibold">Sample: </div>
-                <div className={`color-title ${className}`}>{sampleText}</div>
+                <div className={`color-title`}><a ref={colorLinkRef}  className={className} href="#">{sampleText}</a></div>
+            </div>
+            <div>
+                <div className="small-semibold">Underline: </div>
+                <div className="color-title small">{_underline ? "YES" : "NO"}</div>
             </div>
             <div>
                 <div className="small-semibold">RGBA: </div>
-                <div className="color-title small">{_backgroundColorRGB}</div>
+                <div className="color-title small">{_colorRGB}</div>
             </div>
             <div>
                 <div className="small-semibold">Opacity: </div>
