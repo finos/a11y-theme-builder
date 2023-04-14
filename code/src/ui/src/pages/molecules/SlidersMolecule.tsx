a067cc4d39270129d6dbd71593389ed2636526af
Bryce Curtis <curtis.bryce@gmail.com>
2023-04-14 12:36:47 -0400

    Change properties to be multipliers

--- code/src/ui/src/pages/molecules/SlidersMolecule.tsx
+++ code/src/ui/src/pages/molecules/SlidersMolecule.tsx
@@ -2,24 +2,24 @@
  * Copyright (c) 2023 Discover Financial Services
  * Licensed under MIT License. See License.txt in the project root for license information
  */
-import { InputLabel, MenuItem, Select, Slider } from '@mui/material';
-import React, { useState } from 'react';
-import { NumberSelectable } from '../../components/editors/NumberSelectable';
+import { Slider } from '@mui/material';
+import React from 'react';
+import { NumberScaledSelectable } from '../../components/editors/NumberScaledSelectable';
 import { StringSelectable } from '../../components/editors/StringSelectable';
 import { Sliders } from 'a11y-theme-builder-sdk';
 import { ExampleSection } from '../content/ExampleSection';
 import { GeneratedCodeSection } from '../content/GeneratedCodeSection';
 import { HeadingSection } from '../content/HeadingSection';
 import { SettingsSection } from '../content/SettingsSection';
-
-const name = "SlidersMolecule";
+import { getCssValue } from '../../mui-a11y-tb/themes/Theme';
 
 interface Props {
     slidersMolecule: Sliders;
 }
 
 export const SlidersMolecule: React.FC<Props> = ({ slidersMolecule }) => {
-    // console.log(`${name} - >>> enter()`)
+    const grid = parseInt(getCssValue("--spacing-1"));
+    const radius = parseInt(getCssValue("--radius-1"));
 
     return (
         <>
@@ -32,16 +32,16 @@ export const SlidersMolecule: React.FC<Props> = ({ slidersMolecule }) => {
             </ExampleSection>
             <SettingsSection>
                 <div className="form-row">
-                    <NumberSelectable property={slidersMolecule.handleBorderRadius} units="px" defaultValue={8} />
+                    <NumberScaledSelectable property={slidersMolecule.handleBorderRadius} units="px" defaultValue={1} scale={radius}/>
                 </div>
                 <div className="form-row">
-                    <NumberSelectable property={slidersMolecule.visibleHeight} units="px" defaultValue={32} />
+                    <NumberScaledSelectable property={slidersMolecule.visibleHeight} units="px" defaultValue={3} scale={grid}/>
                 </div>
                 <div className="form-row">
                     <StringSelectable property={slidersMolecule.handleElevation} defaultValue="No Elevation" />
                 </div>
                 <div className="form-row">
-                    <NumberSelectable property={slidersMolecule.barHeight} units="px" defaultValue={32} />
+                    <NumberScaledSelectable property={slidersMolecule.barHeight} units="px" defaultValue={1} scale={grid}/>
                 </div>
                 <div className="form-row">
                     <StringSelectable property={slidersMolecule.barInsetShadow} defaultValue="None" />
