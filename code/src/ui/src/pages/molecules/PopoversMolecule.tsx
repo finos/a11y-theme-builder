8e1db26889dd5c1fc2c0a96b69977f1b49e52372
Evan Kountouris <evangk6@gmail.com>
2023-04-17 13:22:14 -0400

    fix popovers molecule elevations

--- code/src/ui/src/pages/molecules/PopoversMolecule.tsx
+++ code/src/ui/src/pages/molecules/PopoversMolecule.tsx
@@ -28,6 +28,10 @@ export const PopoversMolecule: React.FC<Props> = ({ popoversMolecule, designSyst
         setAnchor(null)
     }
 
+    const boxShadowString = (popoversMolecule.elevation.getValue() === "No Elevation") 
+        ? "none" 
+        : "var(--" + popoversMolecule.elevation.getValue()?.toLowerCase() +") !important"
+
     return (
         <div>
             <HeadingSection item={popoversMolecule} title="Apply Styles"></HeadingSection>
@@ -51,6 +55,7 @@ export const PopoversMolecule: React.FC<Props> = ({ popoversMolecule, designSyst
                     PaperProps={{
                         sx: {
                             borderRadius: "calc(var(--popoverRadius) * var(--radius-1))",
+                            boxShadow: boxShadowString
                         }
                     }}
                 >
