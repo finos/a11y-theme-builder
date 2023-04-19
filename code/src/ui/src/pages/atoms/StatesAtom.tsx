f56bb6628aa0b9f53b5db044bf9bdf7cc8bcaa52
Bryce Curtis <curtis.bryce@gmail.com>
2023-04-19 16:33:53 -0400

    [TB] State settings atom not updating #252

--- code/src/ui/src/pages/atoms/StatesAtom.tsx
+++ code/src/ui/src/pages/atoms/StatesAtom.tsx
@@ -2,7 +2,7 @@
  * Copyright (c) 2023 Discover Financial Services
  * Licensed under MIT License. See License.txt in the project root for license information
  */
-import React, { useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { InputLabel, TextField } from '@mui/material';
 import { StateSettings } from 'a11y-theme-builder-sdk';
 import './StatesAtom.css'
@@ -24,29 +24,45 @@ export const StatesAtom: React.FC<Props> = ({ atom }) => {
     const [_blockPickerColor, _setBlockPickerColor] = useState<string>("#ffffff");
     const [_selectedState, _setSelectedState] = useState<string>("");
 
+    const [info, setInfo] = useState([getCssValue(`--info`), getCssValue(`--dm-info`)]);
+    const [success, setSuccess] = useState([getCssValue(`--success`), getCssValue(`--dm-success`)]);
+    const [warning, setWarning] = useState([getCssValue(`--warning`), getCssValue(`--dm-warning`)]);
+    const [danger, setDanger] = useState([getCssValue(`--danger`), getCssValue(`--dm-danger`)]);
+
+    useEffect(() => {
+        atom.info.prop.setListener("atom", function() {
+            setInfo([getCssValue(`--info`), getCssValue(`--dm-info`)]);
+        })
+        atom.success.prop.setListener("atom", function() {
+            setSuccess([getCssValue(`--success`), getCssValue(`--dm-success`)]);
+        })
+        atom.warning.prop.setListener("atom", function() {
+            setWarning([getCssValue(`--warning`), getCssValue(`--dm-warning`)]);
+        })
+        atom.danger.prop.setListener("atom", function() {
+            setDanger([getCssValue(`--danger`), getCssValue(`--dm-danger`)]);
+        })
+    }, [])
+
+
     const renderExample = (lm?:boolean) => {
-        const mode = lm ? "" : "dm-";
-        let info = getCssValue(`--${mode}info`);
-        let success = getCssValue(`--${mode}success`);
-        let warning = getCssValue(`--${mode}warning`);
-        let danger = getCssValue(`--${mode}danger`);
+        const mode = lm ? 0 : 1;
         return (
             <div style={{paddingRight:"20px"}}>
                 <InputLabel className="label-1">Information</InputLabel>
-                <TextField className="info" value={info}/>
+                <TextField className="info" value={info[mode]}/>
                 <div className="top24"/>
                 <InputLabel className="label-1">Success</InputLabel>
-                <TextField className="success" value={success}/>
+                <TextField className="success" value={success[mode]}/>
                 <div className="top24"/>
                 <InputLabel className="label-1">Warning</InputLabel>
-                <TextField className="warning" value={warning}/>
+                <TextField className="warning" value={warning[mode]}/>
                 <div className="top24"/>
                 <InputLabel className="label-1">Danger</InputLabel>
-                <TextField className="danger" value={danger}/>
+                <TextField className="danger" value={danger[mode]}/>
             </div>
         )
     }
-
 
     return (
         <div className="color-palette-right-content">
