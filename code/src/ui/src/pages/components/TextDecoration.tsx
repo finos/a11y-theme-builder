70c82d2bf5bf9c3949bb9ca1f1e3fd094dfb19d3
Lise Noble <lisenoble@Lises-MBP.lan>
2023-04-29 08:41:40 -0400

    backgrounds and colors

---
+++ code/src/ui/src/pages/components/TextDecoration.tsx
@@ -0,0 +1,26 @@
+﻿/*
+ * Copyright (c) 2023 Discover Financial Services
+ * Licensed under MIT License. See License.txt in the project root for license information
+ */
+import React from 'react';
+import { HeadingSection } from '../content/HeadingSection';
+import { ExampleSection } from '../content/ExampleSection';
+
+interface Props {
+}
+
+export const TextDecoration: React.FC<Props> = () => {
+    return (
+        <div className="content">
+            <HeadingSection title='Desktop' heading='Text Decorations'></HeadingSection>
+            <ExampleSection>
+              <div className="subtitle1">Gradient Title</div>
+              <h1 className="gradient-title">Sample Title</h1>
+
+              <div className="subtitle1">Colored Drop Shadow Title</div>
+              <h1 className="gradient-title">Sample Title</h1>
+
+            </ExampleSection>
+        </div>
+    )
+}
