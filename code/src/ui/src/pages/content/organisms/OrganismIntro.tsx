bb5cfb361c83b93d55a9483c0188fc29aab5a915
Aaron Reed <aaronreed708@gmail.com>
2023-08-31 13:38:52 -0400

    fix some spelling and wording issues w.r.t. organisms

--- code/src/ui/src/pages/content/organisms/OrganismIntro.tsx
+++ code/src/ui/src/pages/content/organisms/OrganismIntro.tsx
@@ -12,7 +12,7 @@ const OrganismIntro: React.FC = () => {
             <h1>Organisms</h1>
             <div>
                 <div>
-                    <p>The the input from the user regaring Atoms and Molecules settings help us create design system styles and components.  These component are combined together to create organisms, which represnt common design paterns with various background colors. We layer the components on the backgrounds to assure accessibility contrast and spacing requirements.</p>
+                    <p>The input that we gather from the user as Atom and Molecule settings help us create design system styles and components.  These components can be combined together to create organisms, which represnt common design patterns with various background colors. In organisms we layer the components on these backgrounds in a way to assure accessibility contrast and spacing requirements are met.</p>
                 </div>
             </div>
         </>
