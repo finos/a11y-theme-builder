0aa3fb5ed7753aec8abe657580e078e0a38aefa6
Aaron Reed <aaronreed708@gmail.com>
2023-09-20 23:36:03 -0400

    discoverfinancial/a11y-theme-builder#612: add device target to Theme Layers

--- code/src/ui/src/components/AccessibilityLayersButton.tsx
+++ code/src/ui/src/components/AccessibilityLayersButton.tsx
@@ -7,6 +7,7 @@ import { Checkbox, FormControl, InputLabel,
     ListItemText, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 import { DesignSystem } from 'a11y-theme-builder-sdk';
 import './AccessibilityLayersButton.css';
+import { StringSelectable } from './editors/StringSelectable';
 
 const name = "AccessibilityLayersButton";
 
@@ -89,6 +90,9 @@ export const AccessibilityLayersButton: React.FC<Props> = ({ designSystem}) => {
                                 <ListItemText primary={prop.name} />
                             </MenuItem>
                         ))}
+                        <MenuItem key={designSystem.layers.deviceTarget.name} value={designSystem.layers.deviceTarget.name}>
+                            <StringSelectable property={designSystem.layers.deviceTarget} variant='radio' defaultValue='desktop' />
+                        </MenuItem>
                     </Select>
                 </FormControl>
             </div>
