c283ea7d775a2cca35a664a6f259e736639afb9c
Alfred Neequaye <alfred.neequaye@turntabl.io>
2023-11-08 08:12:12 +0000

    Update Netlify logo in Left Nav Footer
    The Netlify logo in the LeftNavFooter component has been updated to netlify-light.svg instead of netlify-color-bg.svg. This change makes the logo more visually consistent with the rest of the application's design. The new logo is clearer and easier on the eyes when viewed against the background color of the application's left navigation bar.

--- code/src/ui/src/components/LeftNavFooter.tsx
+++ code/src/ui/src/components/LeftNavFooter.tsx
@@ -4,7 +4,7 @@ export const LeftNavFooter: React.FC = () => {
     return (
         <div className="design-system-editor-left-nav-footer">
             <a href="https://www.netlify.com">
-                <img src="https://www.netlify.com/v3/img/components/netlify-color-bg.svg"
+                <img src="https://www.netlify.com/v3/img/components/netlify-light.svg"
                      alt="Deploys by Netlify"/>
             </a>
         </div>
