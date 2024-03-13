421f5770698208ba05a6cc8c03890435dfed4cfe
Evan Kountouris <evangk6@gmail.com>
2024-03-13 17:57:18 -0400

    get a button working

--- code/src/stories/Button.stories.ts
+++
@@ -1,6 +0,0 @@
-import Button from '@mui/material/Button';
-
-export default {
-    title: "Button",
-    component: Button,
-}
---
+++ code/src/stories/Button.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+export default {
+    title: 'Button',
+    component: Button,
+}
+
+export const primary = () => <Button>This is a Button!</Button>
