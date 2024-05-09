444e89f0bee57762476a0acb516578ce9edad2cf
ravjot07 <ravu2004@gmail.com>
2024-05-10 02:39:19 +0530

    testing

--- code/src/ui/src/mui-a11y-tb/components/Dropdown.tsx
+++ code/src/ui/src/mui-a11y-tb/components/Dropdown.tsx
@@ -1,31 +1,32 @@
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { ReactNode } from 'react';
 import { Select, SelectProps } from '@mui/material';
 
-interface Props extends SelectProps {
-  children?: ReactNode;
-}
+type Props = SelectProps & {
+    children?: ReactNode;
+};
 
 export const Dropdown: React.FC<Props> = ({ children, MenuProps, ...rest }) => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const refContainer = React.useRef(null);
-  useEffect(() => {
-    if (refContainer.current) {
-      setAnchorEl(refContainer.current);
-    }
-  }, [refContainer]);
+    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+    const refContainer = React.useRef<HTMLDivElement>(null);
 
-  return (
-    <div>
-      <Select
-        {...rest}
-        MenuProps={{
-          container: anchorEl,
-          ...MenuProps,
-        }}
-      >
-        {children}
-      </Select>
-      <div ref={refContainer}></div>
-    </div>
-  );
+    React.useEffect(() => {
+        if (refContainer.current) {
+            setAnchorEl(refContainer.current);
+        }
+    }, []);
+
+    return (
+        <div>
+            <Select
+                {...rest}
+                MenuProps={{
+                    container: anchorEl,
+                    ...MenuProps,
+                }}
+            >
+                {children}
+            </Select>
+            <div ref={refContainer}></div>
+        </div>
+    );
 };
